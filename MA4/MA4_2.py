59bc5d7c9e8253004d5d71580bfc4b6d100f1955
IsakI1 <isak.islamovic.3561@student.uu.se>
2024-05-13 20:21:21 +0200

    s

--- MA4/MA4_2.py
+++ MA4/MA4_2.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
-from person import Person
-from person import fib_py
+from person import *
 
 
 def main():
@@ -12,6 +11,9 @@ def main():
 	f.setAge(51)
 	print(f.getAge())
 	print(f.getDecades())
+	f.setAge(20)
+	print(f.fib_py())
+	print(f.fib_numba())
 
 if __name__ == '__main__':
 	main()
