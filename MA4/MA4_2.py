e12cf28350d4d347f31172a607f318f2a5283a11
IsakI1 <isak.islamovic.3561@student.uu.se>
2024-05-13 20:42:47 +0200

    s

--- MA4/MA4_2.py
+++ MA4/MA4_2.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 from person import *
+import time
+from matplotlib import pyplot as plt
 
 
 def main():
@@ -12,7 +14,19 @@ def main():
 	print(f.getAge())
 	print(f.getDecades())
 	f.setAge(20)
-	print(f.fib_py())
+
+	lista_py = []
+	n = [num for num in range(20, 31)]
+
+	for i in n:
+		start = time.perf_counter()
+		print(f.fib_py())
+		end = time.perf_counter()
+		tid = (start-end)
+		lista_py.append(tid)
+	plt.plot(n,lista_py)
+	plt.savefig()
+
 	#print(f.fib_numba())
 
 if __name__ == '__main__':
