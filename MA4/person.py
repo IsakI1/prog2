342dc9f5ffc703730262e73905a3aa0e1930fb31
IsakI1 <isak.islamovic.3561@student.uu.se>
2024-05-14 00:58:51 +0200

    s

--- MA4/person.py
+++ MA4/person.py
@@ -42,5 +42,8 @@ class Person(object):
 		if n <= 1:
 			return n
 		else:
-			return self.fib_numba(n-1) + self.fib_numba(n-2)
+			a, b = 0, 1
+			for _ in range(2, n + 1):
+				a, b = b, a + b
+			return b
 
