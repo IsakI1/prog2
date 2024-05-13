f416793a2348b687a59fff0f126525daf624e66e
IsakI1 <isak.islamovic.3561@student.uu.se>
2024-05-13 23:03:41 +0200

    s

--- MA4/person.py
+++ MA4/person.py
@@ -37,15 +37,11 @@ class Person(object):
 		else:
 			return self.fib_py(n-1) + self.fib_py(n-2)
 		
-	@jit
+	@jit(nopython=True)
 	def fib_numba(self, n=None):
 		if n is None:
 			n = self.getAge()
 		if n <= 1:
 			return n
 		else:
-<<<<<<< HEAD
-			return self.fib_py(n-1) + self.fib_py(n-2)"""
-=======
-			return self.fib_py(n-1) + self.fib_py(n-2)
->>>>>>> 72f31fd34855701cb1e28adbf01da82b8b396a24
+			return self.fib_numba(n - 1) + self.fib_numba(n - 2)
