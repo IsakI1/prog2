08fae512b303d37dc1fdc8e49bddf183fe3023e5
IsakI1 <isak.islamovic.3561@student.uu.se>
2024-05-14 14:16:20 +0200

    s

--- MA4/person.cpp
+++ MA4/person.cpp
@@ -38,7 +38,7 @@ extern "C"{
 	Person* Person_new(int a) {return new Person(a);}
 	int Person_getAge(Person* person) {return person->getAge();}
 	void Person_setAge(Person* person, int a) {person->setAge(a);}
-	int Person_fib_cpp(Person* person, int n) { return person->fib(n); }
+	int Person_fib_cpp(Person* person, int n) { return person->fib_cpp(n); }
 	double Person_getDecades(Person* person) {return person->getDecades();}
 	void Person_delete(Person* person){
 		if (person){
