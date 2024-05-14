0336454198b3851b82cef05e5946c1b7c28643fb
IsakI1 <isak.islamovic.3561@student.uu.se>
2024-05-14 15:02:16 +0200

    s

--- MA4/person.py
+++ MA4/person.py
@@ -41,9 +41,13 @@ class Person(object):
 		else:
 			return self.fib_py(n-1) + self.fib_py(n-2)
 
-	@classmethod
-	@njit(types.int64(types.int64, types.pyobject))
-	def fib_numba(cls,self, n = None):
+	
+	def fib_numba(self):
+		n = self.getAge()
+		return self.fib_numba_helper(n)
+			
+	@njit
+	def fib_numba_helper(self, n: int) -> int:
 		if n is None:
 			n = self.getAge()
 		if n <= 1:
